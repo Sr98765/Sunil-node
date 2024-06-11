1b85c29f1c57f658a5fa2be432c8a53c3da27f7b
SR <sunil2019masters@gmail.com>
2024-06-11 19:49:15 +0000

    Storybook with card component

---
+++ my-react-1/src/stories/Card.stories.js
@@ -0,0 +1,23 @@
+// src/stories/Card.stories.js
+import React from "react";
+import Card from "../Card"; // Correct import path
+
+export default {
+  title: "Card",
+  component: Card,
+};
+
+const Template = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: "Card Title",
+  content: "This is the card content.",
+};
+
+export const WithLongerContent = Template.bind({});
+WithLongerContent.args = {
+  title: "Card Title",
+  content:
+    "This is the card content. It has a bit more text to show how the card handles longer content.",
+};
