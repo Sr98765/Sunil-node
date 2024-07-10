7beb0d4eb8e3fc2b04b40fd78a545f9ced16b393
Sr98765 <sunil2019masters@gmail.com>
2024-07-10 09:40:30 +0000

    co

---
+++ my-react-1/src/Components/ContactForm/ContactForm.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+
+export default {
+  title: 'ContactForm',
+  component: ContactForm,
+};
+
+export const Default = () => <ContactForm />;
